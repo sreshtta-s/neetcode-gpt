da41262adcc9d2e266821811600a033b8d4abf3a
Sreshtta S Sreeraj <sreshttassreeraj@gmail.com>
2026-07-28 11:59:45 +0530

    Update MLP from scratch (Build a Neural Net 4/5)

--- foundations/mlp.py
+++ foundations/mlp.py
@@ -10,10 +10,10 @@ class Solution:
         # h[0] = np.maximum(np.dot(x,weights[0]+biases[0]))
         for i in range (0,(len(weights)-1)):
       
-            x = np.dot(x, weights[i]) + biases[i]
+            x = (x @ weights[i]) + biases[i]
 
             x = np.maximum(0, x)
-        z = np.dot(x,weights[-1])+biases[-1]
+        z = (x@weights[-1])+biases[-1]
 
 
         # weights: list of 2D weight matrices
